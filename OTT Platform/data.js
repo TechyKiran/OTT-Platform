fde7e296bcd2cac2a37aed5a7876c5c76db1d0fd
TechyKiran <155069074+TechyKiran@users.noreply.github.com>
2024-02-02 22:25:28 +0530

    Add files via upload

---
+++ OTT Platform/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Pro Kabaddi',
+        des: 'The Pro Kabaddi League 2023 Season 10 officially began on 2 December and is scheduled to conclude on 21 February 2024',
+        image: 'prokabaddi.jpg'
+    },
+    {
+        name: 'Pro Kabaddi',
+        des: 'The Pro Kabaddi League 2023 Season 10 officially began on 2 December and is scheduled to conclude on 21 February 2024',
+        image: 'prokabaddi.jpg'
+    },
+    {
+        name: 'Pro Kabaddi',
+        des: 'The Pro Kabaddi League 2023 Season 10 officially began on 2 December and is scheduled to conclude on 21 February 2024',
+        image: 'prokabaddi.jpg'
+    },
+    {
+        name: 'Pro Kabaddi',
+        des: 'The Pro Kabaddi League 2023 Season 10 officially began on 2 December and is scheduled to conclude on 21 February 2024',
+        image: 'prokabaddi.jpg'
+    },
+    {
+        name: 'Pro Kabaddi',
+        des: 'The Pro Kabaddi League 2023 Season 10 officially began on 2 December and is scheduled to conclude on 21 February 2024',
+        image: 'prokabaddi.jpg'
+    }
+]
